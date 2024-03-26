a8f536b653e191a0e85949e56c51d98eb4128d00
jakub-wasik <jakub.wasik@schibsted.com>
2024-03-26 19:18:16 +0100

    Day 8

---
+++ typescript/day-08/part-2.ts
@@ -0,0 +1,107 @@
+import { loadInput } from "../utils";
+
+
+// Todo: put test_input into a "test" file.
+const test_input = `RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)`;
+
+const test_input2 = `LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)`;
+
+
+// const input = test_input;
+// const input = test_input2;
+const input = loadInput("input");
+const [directions, ...rows] = input.split("\n").filter(Boolean);
+
+type Node = {
+  value: string;
+  left: string;
+  right: string;
+};
+
+const toNode = (row: string): Node => {
+  const [value, branches] = row.split(" = ");
+  const [left, right] = branches.replace("(", "").replace(")", "").split(", ");
+  return { value, left, right };
+};
+
+// const nodes = rows.map(toNode).reduce<Record<string, Node>>((acc, node) => {
+//   acc[node.value] = node;
+//   return acc;
+// }, {});
+
+const nodes = new Map<string, Node>(
+  rows.map(toNode).map((node) => [node.value, node])
+);
+
+const getNode = (key: string): Node => {
+  const node = nodes.get(key);
+  if (node === undefined) throw new Error(`Could not find node: ${key}`);
+  return node;
+};
+
+type Direction = "left" | "right";
+const toDirection = (text: string): Direction => {
+  switch (text.charAt(0)) {
+    case "L": return "left";
+    case "R": return "right";
+    default: throw new Error(`Unknown direction: ${text.charAt(0)}`);
+  }
+};
+
+const directionGenerator = (directions: string): (() => Direction) => {
+  let i = 0;
+  const len = directions.length;
+
+  return () => {
+    const dir = toDirection(directions[i]);
+    i = (i+1) % len;
+    return dir;
+  };
+};
+
+const getNextDirection = directionGenerator(directions);
+
+const countSteps = (start: string) => {
+  let current = getNode(start);
+  let steps = 0;
+
+  // console.log(nodes);
+  // console.log(directions);
+  // console.log("Start: ", directions[0], current);
+
+  while (!current.value.endsWith("Z")) {
+    steps++;
+    const direction = getNextDirection();
+    const nextNode = getNode(current[direction]);
+    
+    // console.log({ steps, direction, current, nextNode });
+
+    current = nextNode;
+  }
+
+  // console.log({ steps, current });
+  return steps;
+};
+
+const startingKeys = [...nodes.keys()].filter(k => k.endsWith("A"));
+
+const steps = startingKeys.map(countSteps);
+
+const gcd = (a: number, b: number): number => !b ? a : gcd(b, a % b);
+const lcm = (a: number, b: number) => (a * b) / gcd(a, b);   
+
+const z = steps.reduce((acc, step) => lcm(acc, step), Math.min(...steps));
+
+console.log(z);
